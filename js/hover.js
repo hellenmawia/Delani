15c2631eed53747931ea5cad2083dbe4102b1f1c
Hellen Mawia <hellen.mawia@gmail.com>
2022-02-04 11:39:32 +0300

    added toggle effect on what we do

--- js/hover.js
+++ js/hover.js
@@ -1,36 +1,40 @@
-
-   $('.portimg').hover(function(){
-    $('.porttext',this).slideToggle('slow');
- }, function(){
-    $('.porttext',this).slideToggle('slow');
- });
-
- $(".design").click(function(event) {
-    $(".show1").slideToggle();
-    $(".switchdisplay").slideToggle();
+$(document).ready(function(){
+    $('.portimg').hover(function(){
+        $('.porttext',this).slideToggle('slow');
+     }, function(){
+        $('.porttext',this).slideToggle('slow');
+     });
+    
+     $("#design").click(function() {
+        $("#show1").slideToggle();
+        $("#hidedesign").slideToggle();
+      });
+    
+      $("#development").click(function(event) {
+        $("#show2").slideToggle();
+        $("#hidedevelopment").slideToggle();
+      });
+    
+      $("#product").click(function(event) {
+        $("#show3").slideToggle();
+        $("#hideproduct").slideToggle();
+      });
+    
+     $("form#form").on('submit',function(event){
+        event.preventDefault();
+        let name = $("input#name").val();
+        let email = $("input#email").val();
+        let message = $("textarea#message").val();
+    
+        if ($("input#name").val() && $("input#email").val()){
+            alert ("Hello " + name + ", Thank you for reaching out,your message has been received");
+        }
+        else {
+            alert("Please provide your correct name and email!");
+        }
+    
+    });
+    
   });
 
- $("form#form").on('submit',function(event){
-    event.preventDefault();
-    let name = $("input#name").val();
-    let email = $("input#email").val();
-    let message = $("textarea#message").val();
-
-    if ($("input#name").val() && $("input#email").val()){
-        alert ("Hello " + name + ", Thank you for reaching out,your message has been received");
-    }
-    else {
-        alert("Please provide your correct name and email!");
-    }
-
-});
-
-
-
-
-
-
-
-
-
-
+   
